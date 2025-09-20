b88124923e77340d2ce2100a0a7dba5464ffc731
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-20 09:01:08 -0500

    add : arrays frutas

--- clase 1/vitony-project/src/ejercicios/04-desestructurador.ts
+++ clase 1/vitony-project/src/ejercicios/04-desestructurador.ts
@@ -2,7 +2,7 @@
 interface Details {
     autor: string,
     year: number,
-}
+};
 
 
 interface ReproductorAudio {
@@ -10,7 +10,43 @@ interface ReproductorAudio {
     duration: number;
     song: string;
     detail: Details
-}
+};
+
+const reproductorAudio : ReproductorAudio = {
+    volume: 50,
+    duration: 10,
+    song: "Testing song",
+    detail: {
+        autor: "Pepito",
+        year: 2025,
+    }
+};
+
+// const song1 = reproductorAudio.song;
+// const autor = reproductorAudio.detail.autor;
+
+// Desestructurador
+const { song: nombreCancion, duration : duracionCancion} = reproductorAudio;
+console.log({nombreCancion, duracionCancion})
+
+// Ejemplo deconstructores anidados
+// 1. 
+const { detail } = reproductorAudio;
+const { autor : nombreAutor1 } = detail;
+// 2. 
+const { autor: nombreAutor2 } = reproductorAudio.detail;
+// 3. 
+const { detail:{autor: nombreAutor3}} = reproductorAudio;
+// imprimir
+console.log({nombreAutor1, nombreAutor2, nombreAutor3});
+
+// Arrays
+let frutas = ["Peras", "Manzanas", "Fresas"];
+console.log("Frutas: " + (frutas[3] || "No se ha encontrado."));
+
+const [f1, f2, f3, f4 = "No se ha encontrado"] = frutas;
+const [,,fruta3] = frutas;
+console.log({f4, fruta3})
 
 
 export {};
--- clase 1/vitony-project/src/main.ts
+++ clase 1/vitony-project/src/main.ts
@@ -1,12 +1,12 @@
 import './style.css'
-import './ejercicios/01-tipos-datos.ts'
-import './ejercicios/02-objetos-arreglos.ts'
-import './ejercicios/03-funciones.ts'
+// import './ejercicios/01-tipos-datos.ts'
+// import './ejercicios/02-objetos-arreglos.ts'
+// import './ejercicios/03-funciones.ts'
 import './ejercicios/04-desestructurador.ts'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = 'Hamborgruesa';
-console.log('Holaaa pizza')
+console.log('--- FIN ---')
 
 // npm install
 // npm run devbbbbb
