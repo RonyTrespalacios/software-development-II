8aeaf2e1caabead939d2306de8a0c7ff5a1777c5
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-20 09:42:35 -0500

    add : ejercicio ts productos, impuestos y propinas

--- clase 1/vitony-project/src/ejercicios/04-desestructurador.ts
+++ clase 1/vitony-project/src/ejercicios/04-desestructurador.ts
@@ -48,5 +48,57 @@ const [f1, f2, f3, f4 = "No se ha encontrado"] = frutas;
 const [,,fruta3] = frutas;
 console.log({f4, fruta3})
 
+// ------------------------------------------------------------------
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone : Product = {
+    description: "Samsung A54",
+    price: 100,
+}
+
+const laptop : Product = {
+    description: "Lenovo",
+    price: 300
+}
+
+interface optionTaxes {
+    taxes: number,
+    products: Product[],
+    tip?: number, 
+}
+
+function calcTaxes(options: optionTaxes): number[]{
+
+    const {products} = options;
+    let totalPrice : number = 0;
+    // products.forEach({price : precioProducto} => {
+    products.forEach(element => {
+        let { price : precioProducto } = element;
+        totalPrice += precioProducto;
+    });
+    const taxes = totalPrice*options.taxes;
+    const tips = totalPrice*(options.tip||0);
+    return [totalPrice, taxes, tips];
+}
+
+const shoppingBag : Product[] = [phone, laptop];
+const taxesPercentage: number = 0.19;
+const tipsPercentage: number = 0.1;
+
+const [totalPrice, totalTaxes, totalTips] = calcTaxes({
+    taxes: taxesPercentage,
+    products: shoppingBag,
+    tip: tipsPercentage,
+})
+
+console.log(shoppingBag)
+console.log(`Precio productos: $${totalPrice}.`)
+console.log(`Impuestos totales: $${totalTaxes}.`)
+console.log(`Propina: $${totalTips}.`)
+console.log(`Grand Total: $${totalPrice+totalTaxes+totalTips}.`)
+
 
 export {};
