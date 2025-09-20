35d1fedc934d50e1ac2ec84e0f753587697e4f7f
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-19 21:20:09 -0500

    ejercicio 1 interfaz Usuario

--- clase 1/vitony-project/src/ejercicios/03-funciones.ts
+++ clase 1/vitony-project/src/ejercicios/03-funciones.ts
@@ -28,9 +28,9 @@ console.log(result3);
 /// -------------------- ///
 
 interface PersonajeInterface {
-    nombre: string;
-    vida: number;
-    mostrarDetalle: () => void;
+    nombre: string,
+    vida: number,
+    mostrarDetalle: () => void,
 }
 
 const healthPercentage = (personaje:PersonajeInterface, valor:number) => {
@@ -41,14 +41,54 @@ const persona : PersonajeInterface = {
     nombre: "Rony",
     vida: 88,
     mostrarDetalle(){
-        console.log(`Nombre -> ${this.nombre}, vida -> ${this.vida}`)
-    }
-}
+        console.log(`Nombre -> ${this.nombre}, vida -> ${this.vida}`);
+    },
+};
 
-console.log(persona.mostrarDetalle())
+console.log(persona.mostrarDetalle());
 
 healthPercentage(persona,10);
 
-console.log(persona.mostrarDetalle())   
+console.log(persona.mostrarDetalle());
+
+
+// Ejercicio de implementar la interfaz para lo siguiente: 
+
+
+interface DireccionInterface {
+    ciudad: string,
+    pais: string,
+};
+
+interface Usuario {
+    id: number,
+    nombre: string,
+    email: string,
+    activo: boolean,
+    roles: string[],
+    direccion: DireccionInterface,
+    saludar: () => string,
+    actualizarEmail: (nuevoEmail : string) => void,
+};
+
+const usuario1: Usuario = {
+    id: 1,
+    nombre: "Ana Pérez",
+    email: "ana@example.com",
+    activo: true,
+    roles: ["admin", "editor"],
+    direccion: {
+        ciudad: "Bogotá",
+        pais: "Colombia",
+    },
+    saludar():string{
+        return `Hola, mi nombre es ${this.nombre}!`
+    },
+    actualizarEmail(nuevoEmail : string) : void{
+        this.email = nuevoEmail;
+    }
+};
+
+console.log(usuario1)
 
 export {};
