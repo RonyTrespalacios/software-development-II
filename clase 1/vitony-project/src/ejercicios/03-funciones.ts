4f0184b429280ad71bddf7a987df7a2dca3374b5
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-19 21:07:52 -0500

    add : funciones interfaces y atributos

--- clase 1/vitony-project/src/ejercicios/03-funciones.ts
+++ clase 1/vitony-project/src/ejercicios/03-funciones.ts
@@ -18,4 +18,37 @@ const sumar2 = (n1 : number, n2 : number) : number => {
 let result2 : number = sumar2(1,2);
 console.log({result, result2});
 
+function multiplicar(n1:number, n2:number=1, n3?:number) : number {
+    return n1*n2*(n3||1);
+}
+
+let result3 : number = multiplicar(2,2,2)
+console.log(result3);
+
+/// -------------------- ///
+
+interface PersonajeInterface {
+    nombre: string;
+    vida: number;
+    mostrarDetalle: () => void;
+}
+
+const healthPercentage = (personaje:PersonajeInterface, valor:number) => {
+    personaje.vida += valor;
+}
+
+const persona : PersonajeInterface = {
+    nombre: "Rony",
+    vida: 88,
+    mostrarDetalle(){
+        console.log(`Nombre -> ${this.nombre}, vida -> ${this.vida}`)
+    }
+}
+
+console.log(persona.mostrarDetalle())
+
+healthPercentage(persona,10);
+
+console.log(persona.mostrarDetalle())   
+
 export {};
--- clase 1/vitony-project/src/main.ts
+++ clase 1/vitony-project/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import './ejercicios/01-tipos-datos.ts'
 import './ejercicios/02-objetos-arreglos.ts'
+import './ejercicios/03-funciones.ts'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = 'Hamborgruesa';
