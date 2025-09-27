354bd1fe5075a1e1b56605851002adb60de7e1ad
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-27 08:49:13 -0500

    feat : importing counter component

---
+++ clase 2/angular-rony-project/src/app/pages/counter/counter-page.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    template: `
+    <h1> Hola Pizza!</h1>
+    `
+})
+
+export class CounterPageComponent {
+    counter : number = 0; 
+}
