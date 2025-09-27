0a615b6908f1932f6f109da24862875155ccd302
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-27 09:32:15 -0500

    add : signal aproach

--- clase 2/angular-rony-project/src/app/pages/counter/counter-page.ts
+++ clase 2/angular-rony-project/src/app/pages/counter/counter-page.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 
 @Component({
@@ -7,14 +7,18 @@ import { Component } from "@angular/core";
 })
 export class CounterPageComponent {
     counter : number = 0; 
+    counterSignal = signal(0);
 
     add(value: number){
         this.counter += value;
+        this.counterSignal.update((currentValue)=>currentValue+value);
     }
     decrease(value: number){
         this.counter -= value;
+        this.counterSignal.update((currentValue)=>currentValue-value);
     }
     reset(){
         this.counter = 0;
+        this.counterSignal.set(0);
     }
 }
