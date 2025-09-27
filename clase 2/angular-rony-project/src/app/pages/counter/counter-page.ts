771607d12f0d3b497361a248520f775d8f9efca5
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-27 09:37:20 -0500

    feat : signal example of use

--- clase 2/angular-rony-project/src/app/pages/counter/counter-page.ts
+++ clase 2/angular-rony-project/src/app/pages/counter/counter-page.ts
@@ -1,13 +1,24 @@
-import { Component, signal } from "@angular/core";
+import { CurrencyPipe } from "@angular/common";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { delay } from "rxjs";
 
 
 @Component({
     templateUrl: './counter-page.html',
-    styleUrl: './counter-page.css',    
+    styleUrl: './counter-page.css', 
+    changeDetection: ChangeDetectionStrategy.OnPush,   
 })
 export class CounterPageComponent {
     counter : number = 0; 
     counterSignal = signal(0);
+
+    constructor() {
+        setInterval(()=>{
+            this.counter += 1;
+            this.counterSignal.update((currentValue)=>currentValue+1); // est linea es la que actualiza el reenderizado
+            console.log("Se actualizo el counter.");
+        }, 1000)
+    }
 
     add(value: number){
         this.counter += value;
