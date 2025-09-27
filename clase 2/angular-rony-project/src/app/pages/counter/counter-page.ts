32a76e5b98106e643630978cc9917b53d2a9d807
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-27 09:10:38 -0500

    feat : developing counter component

--- clase 2/angular-rony-project/src/app/pages/counter/counter-page.ts
+++ clase 2/angular-rony-project/src/app/pages/counter/counter-page.ts
@@ -2,11 +2,19 @@ import { Component } from "@angular/core";
 
 
 @Component({
-    template: `
-    <h1> Hola Pizza!</h1>
-    `
+    templateUrl: './counter-page.html',
+    styleUrl: './counter-page.css',    
 })
-
 export class CounterPageComponent {
     counter : number = 0; 
+
+    add(value: number){
+        this.counter += value;
+    }
+    decrease(value: number){
+        this.counter -= value;
+    }
+    reset(){
+        this.counter = 0;
+    }
 }
