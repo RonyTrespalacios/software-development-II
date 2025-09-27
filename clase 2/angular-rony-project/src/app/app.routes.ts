f5e460a8929b7a9543c8d0fd568c6481d8fbfc8b
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-09-27 10:05:35 -0500

    feat : computed signals

--- clase 2/angular-rony-project/src/app/app.routes.ts
+++ clase 2/angular-rony-project/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page';
+import { CarComponent } from './pages/car/car';
 
 export const routes: Routes = [
     {
         path: '',
         component: CounterPageComponent,
-    }
+    },
+    {
+        path: 'car',
+        component: CarComponent,
+    },
 ];
