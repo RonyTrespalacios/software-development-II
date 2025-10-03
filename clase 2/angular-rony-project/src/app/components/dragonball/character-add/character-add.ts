60ed054838f4259998ec9a442f85406fb623f083
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2025-10-03 18:39:32 -0500

    add : character-add component

---
+++ clase 2/angular-rony-project/src/app/components/dragonball/character-add/character-add.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-add',
+  imports: [],
+  templateUrl: './character-add.html',
+  styles: ``
+})
+export class CharacterAdd {
+
+}
